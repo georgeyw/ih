973da3d02ce6ab00f9f403fa7420526cffaa2db5
George Wang <gwang24@gmail.com>
2023-11-18 13:53:25 -0600

    Add HF checkpointing to training loop

--- src/train.py
+++ src/train.py
@@ -5,17 +5,36 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from transformer_lens.utils import lm_cross_entropy_loss
 
+from api_utils import create_hf_repo
+from api_utils import upload_hf_model
+from api_utils import upload_hf_model_configs
+from constants import CHECKPOINT_EVERY
+from constants import CHECKPOINT_ZERO_PAD
 from constants import LOG_EVERY
 from dgp import build_dgp_for_model
 from model import build_model
 
 
 # TODO(george): add wandb logging
+# TODO(george): add HF checkpointing
 def train(model_config_name: str,
           dgp_config_name: str,
           train_config_name: str,
+          run_name: str = None,
+          save_checkpoints: bool = False,
           device: str = 'cuda'):
+    if save_checkpoints:
+        if run_name is None:
+            raise ValueError("Must provide run_name if saving checkpoints")
+        create_hf_repo(run_name)
+        upload_hf_model_configs(model_name=run_name, 
+                                model_config_name=model_config_name, 
+                                dgp_config_name=dgp_config_name, 
+                                train_config_name=train_config_name)
+    
     train_config = _read_train_config(train_config_name)
+    if train_config['seed'] is not None:
+        torch.manual_seed(train_config['seed'])
     model = build_model(model_config_name)
     dgp = build_dgp_for_model(model, dgp_config_name)
     dataset = dgp.generate_dataset(_num_samples(train_config))
@@ -23,7 +42,13 @@ def train(model_config_name: str,
     optimizer = torch.optim.AdamW(model.parameters(),
                                   lr=train_config['lr'],
                                   weight_decay=train_config['weight_decay'])
-    losses = _training_loop(model, optimizer, loader, train_config, device=device)
+    losses = _training_loop(model, 
+                            optimizer, 
+                            loader, 
+                            train_config, 
+                            run_name=run_name,
+                            save_checkpoints=save_checkpoints, 
+                            device=device)
     return losses
 
 
@@ -31,6 +56,8 @@ def _training_loop(model: nn.Module,
                    optimizer: torch.optim.Optimizer,
                    data_loader: DataLoader,
                    train_config: dict,
+                   run_name: str = None,
+                   save_checkpoints: bool = False,
                    device: str = 'cuda'):
     losses = []
     for epoch in range(train_config['epochs']):
@@ -45,6 +72,8 @@ def _training_loop(model: nn.Module,
             losses.append(loss.item())
             if c % LOG_EVERY == 0:
                 print(f"Step: {c}, Loss: {loss.item():.4f}")
+            if save_checkpoints and c % CHECKPOINT_EVERY == 0:
+                upload_hf_model(model, run_name, checkpoint_name=f"checkpoint_{c:0>{CHECKPOINT_ZERO_PAD}d}")
             if c > train_config['max_steps']:
                 break
     return losses
