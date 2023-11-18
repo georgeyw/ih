ed98f911c6a12c00b84bc3f10b0a59e2e3f9f0f7
George Wang <gwang24@gmail.com>
2023-11-18 14:10:28 -0600

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='ih',
     version='0.1.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    include_package_data=True,  # This line is important
+    include_package_data=True,
     package_data={
-        'ih': ['*.json'],
-    }
+        'ih': ['train_configs/*.json',
+               'model_configs/*.json',
+               'dgp_configs/*.json'],
+    },
+    install_requires=requirements
 )
